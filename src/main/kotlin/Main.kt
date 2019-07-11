297a039b695e7d734ad9d34f26c68b0ec5d6749e
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-07-11 16:37:23 +0400

    IF ELSE IF ELSE - WHEN

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,49 @@
 fun main(args: Array<String>) {
-    println("Hello World")
+
+    // ---------------- IF CONDITION ----------------
+    val lives = 2
+    var isGameOver = (lives < 1) // isGameOver is a boolean.
+
+    println("isGameOver --> $isGameOver")
+
+    if(isGameOver){
+        println("Game Over")
+    }else {
+        println("Number of life left = $lives")
+    }
+
+
+    // ---------------- IF-ELSE IF-ELSE ----------------
+    println("How old are you?")
+    // This is used to get the value from the commmand line and convert it to Int.
+    //If you input a non Int, you will get a NumberFormatException
+    var age = readLine()!!.toInt()
+    println("Your age is $age")
+
+    var message : String
+
+    if(age < 18){
+        message = "You are too young -_-"
+    }else if(age > 150){
+        message = "You are dead !! :'( "
+    }else{
+        message = "You are IN !! :D"
+    }
+    println(message)
+
+    // ---------------- WHEN ----------------
+    println("************ starting test2....")
+    println("How old are you?")
+    // This is used to get the value from the commmand line and convert it to Int.
+    //If you input a non Int, you will get a NumberFormatException
+    age = readLine()!!.toInt()
+    println("Your age is $age")
+
+    message = when {
+        age < 18 -> "You are too young -_-"
+        age > 150 -> "You are dead !! :'( "
+        else -> "You are IN !! :D"
+    }
+
+    println(message)
 }
