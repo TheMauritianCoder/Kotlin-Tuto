8e9fbbb61ffb6e961854490f88bea482266abe72
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-07-11 16:16:01 +0400

    Variables + Types + Concatenation + Interpolation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,28 +2,28 @@ fun main(args: Array<String>) {
     println("Hello World")
 
     // Init on declaration.
-    var firstname : String = "Ravi"
+    val firstname : String = "Ravi"
     println(firstname)
 
     // Init on assignment.
-    var lastname : String
+    val lastname : String
     lastname = "Kowlessur"
     println(lastname)
 
     // Numeric type
-    var age : Int = 28
+    val age : Int = 28
     println("Age --> ${age}")
 
-    var monthlyBasicSalary: Int = 1000
-    var monthlyTransport: Int = 550
+    val monthlyBasicSalary: Int = 1000
+    val monthlyTransport: Int = 550
     println("Gross Salary --> ${monthlyBasicSalary + monthlyTransport}")
 
     // Note: Int can only store whole numbers or integers same as Java.
     println("Division Test 1 --> ${monthlyBasicSalary / monthlyTransport}")
 
     // Note: To get the decimals, Double is used. There's Float, but double is the most commonly used.
-    var num1 : Int = 10
-    var num2 : Double = 3.0 // .0 or .<anything> is mandatory
+    val num1 : Int = 10
+    val num2 : Double = 3.0 // .0 or .<anything> is mandatory
     println("Division Test 2 --> ${num1 / num2}")
 
     println("Division Test 2 Concat. --> "+(num1 / num2)) // String concatenation
@@ -31,4 +31,21 @@ fun main(args: Array<String>) {
     println("Division Test 2 Interp. Variable --> $num1") // String Interpolation with variable
     println("Division Test 2 Interp. Cal --> ${num1 / num2}") // String Interpolation with a calculation
 
+    // Difference between Val and Var
+    // val --> Stands for Value --> Immutable --> i.e cannot be changed
+    // var --> Stands for Variable --> Stands for variable --> i.e. Value can change.
+    // As a rule of thumb, always use val unless you know the value will change.
+    var x : Int = 0
+    println("x --> $x")
+    x = 1
+    println("x --> $x")
+
+    val y : Int = 0
+    /* y = 1 // This will now give an error because you cannot chane the value of val. */
+    println("y --> $y")
+
+    // You don't explicitly have to specify the type. Kotlin is intelligent enough to imply it by itself.
+    val stringVal = "This is a string variable"
+    val intVal = 1
+    val doubleVal = 1.0
 }
