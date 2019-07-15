c6de10b49619b3dc76b535ba58b0d7fe4dbfb021
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-07-15 18:19:31 +0400

    Do While Loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,21 @@
 fun main(args: Array<String>) {
 
-    val ravi = Player("Ravi",1)
-    ravi.pickLoot(Loot("Invisibility",LootType.POTION,4.0))
-    ravi.pickLoot(Loot("Mithril",LootType.ARMOR,183.0))
-    ravi.pickLoot(Loot("Ring of Speed",LootType.RING,25.0))
-    ravi.pickLoot(Loot("Red Potion",LootType.POTION,2.0))
-    ravi.pickLoot(Loot("Cursed Shield",LootType.ARMOR,-8.0))
-    ravi.pickLoot(Loot("Brass Ring",LootType.RING,1.0))
-    ravi.pickLoot(Loot("Chain Mail",LootType.ARMOR,4.0))
-    ravi.pickLoot(Loot("Gold Ring",LootType.RING,12.0))
-    ravi.pickLoot(Loot("Health Potion",LootType.POTION,3.0))
-    ravi.pickLoot(Loot("Silver Ring",LootType.RING,6.0))
-
-    ravi.showInventory()
-
     val dracula = VampireKing("Dracula")
     println(dracula)
 
-
-    for(i in 1..10){ // This is for loop with a range of values.
-        while(dracula.lives > 0){
-            if(dracula.dodges()){
-                continue
-            }
-            if(dracula.runAway()){
-                println("Dracula ran away...")
-                break
-            }else{
-                dracula.takeDamage(12)
-            }
+    // In Do While loop, the loop is executed at least once.
+    dracula.lives = 0
+    do {
+        if (dracula.dodges()) {
+            dracula.lives += 1
+            continue
         }
-        println("---------------------------------------------")
-    }
+        if (dracula.runAway()) {
+            println("Dracula ran away...")
+            break
+        } else {
+            dracula.takeDamage(12)
+        }
+    } while (dracula.lives > 0)
+    println("---------------------------------------------")
 }
